33e8596de6b98b3c79eb6b06e1384f0097e9c88b
Thomas Carpenter <thomascarpenter@Thomass-MacBook-Pro.local>
2017-07-19 21:38:46 -0700

    push dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react';
-import {Table, Grid, Segment, Card, Accordion, Icon, Button, Image, List, } from 'semantic-ui-react';
+import {Table, Grid, Segment, Card, Accordion, Icon, Button, Image, List,  } from 'semantic-ui-react';
+
 
 const Dashboard = () => (
   <Card.Group>
     <Card>
       <Card.Content>
         <Card.Header>ACTIVE</Card.Header>
-        <Card.Meta>44</Card.Meta>
-        <Card.Description>Currect Flying Club members</Card.Description>
+        <Card.Meta className="activeUsers"></Card.Meta>
       </Card.Content>
        <Accordion>
     <Accordion.Title>
       <Icon name='dropdown' />
-      More Info?
+      Members
     </Accordion.Title>
     <Accordion.Content>
       <List divided verticalAlign='middle'>
@@ -60,14 +60,13 @@ const Dashboard = () => (
 
     <Card>
       <Card.Content>
-        <Card.Header content='Jake Smith' />
-        <Card.Meta content='Musicians' />
-        <Card.Description content='Jake is a drummer living in New York.' />
+        <Card.Header>INACTIVE</Card.Header>
+        <Card.Meta className="inactiveUser"></Card.Meta> 
       </Card.Content>
        <Accordion>
     <Accordion.Title>
       <Icon name='dropdown' />
-      More Info?
+      Memebrs
     </Accordion.Title>
     <Accordion.Content>
       <List divided verticalAlign='middle'>
@@ -113,123 +112,14 @@ const Dashboard = () => (
     </Card>
 
     <Card>
-      <Card.Content
-        header='Elliot Baker'
-        meta='Friend'
-        description='Elliot is a music producer living in Chicago.'
-      />
+    <Card.Content>
+      <Card.Header>STUDENT</Card.Header>
+      <Card.Meta className="studentUser"></Card.Meta>
+    </Card.Content>  
        <Accordion>
     <Accordion.Title>
       <Icon name='dropdown' />
-      More Info?
-    </Accordion.Title>
-    <Accordion.Content>
-      <List divided verticalAlign='middle'>
-        <List.Item>
-          <List.Content floated='right'>
-            <Button>View</Button>
-          </List.Content>
-          <Image avatar src='../public/dist/img/avatar.png' />
-          <List.Content>
-            Lena
-          </List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Content floated='right'>
-            <Button>View</Button>
-          </List.Content>
-          <Image avatar src='/assets/images/avatar/small/lindsay.png' />
-          <List.Content>
-            Lindsay
-          </List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Content floated='right'>
-            <Button>View</Button>
-          </List.Content>
-          <Image avatar src='/assets/images/avatar/small/mark.png' />
-          <List.Content>
-            Mark
-          </List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Content floated='right'>
-            <Button>View</Button>
-          </List.Content>
-          <Image avatar src='/assets/images/avatar/small/molly.png' />
-          <List.Content>
-            Molly
-          </List.Content>
-        </List.Item>
-      </List>
-    </Accordion.Content>
-    </Accordion>
-    </Card>
-        <Card>
-      <Card.Content>
-        <Card.Header>Matthew Harris</Card.Header>
-        <Card.Meta>Co-Worker</Card.Meta>
-        <Card.Description>Matthew is a pianist living in Nashville.</Card.Description>
-      </Card.Content>
-       <Accordion>
-    <Accordion.Title>
-      <Icon name='dropdown' />
-      More Info?
-    </Accordion.Title>
-    <Accordion.Content>
-      <List divided verticalAlign='middle'>
-        <List.Item>
-          <List.Content floated='right'>
-            <Button>View</Button>
-          </List.Content>
-          <Image avatar src='../public/dist/img/avatar.png' />
-          <List.Content>
-            Lena
-          </List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Content floated='right'>
-            <Button>View</Button>
-          </List.Content>
-          <Image avatar src='/assets/images/avatar/small/lindsay.png' />
-          <List.Content>
-            Lindsay
-          </List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Content floated='right'>
-            <Button>View</Button>
-          </List.Content>
-          <Image avatar src='/assets/images/avatar/small/mark.png' />
-          <List.Content>
-            Mark
-          </List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Content floated='right'>
-            <Button>View</Button>
-          </List.Content>
-          <Image avatar src='/assets/images/avatar/small/molly.png' />
-          <List.Content>
-            Molly
-          </List.Content>
-        </List.Item>
-      </List>
-    </Accordion.Content>
-    </Accordion>
-    </Card>
-
-
-    <Card>
-      <Card.Content>
-        <Card.Header content='Jake Smith' />
-        <Card.Meta content='Musicians' />
-        <Card.Description content='Jake is a drummer living in New York.' />
-      </Card.Content>
-       <Accordion>
-    <Accordion.Title>
-      <Icon name='dropdown' />
-      More Info?
+      Members
     </Accordion.Title>
     <Accordion.Content>
       <List divided verticalAlign='middle'>
@@ -275,68 +165,14 @@ const Dashboard = () => (
     </Card>
 
     <Card>
-      <Card.Content
-        header='Elliot Baker'
-        meta='Friend'
-        description='Elliot is a music producer living in Chicago.'
-      />
-       <Accordion>
-    <Accordion.Title>
-      <Icon name='dropdown' />
-      More Info?
-    </Accordion.Title>
-    <Accordion.Content>
-      <List divided verticalAlign='middle'>
-        <List.Item>
-          <List.Content floated='right'>
-            <Button>View</Button>
-          </List.Content>
-          <Image avatar src='../public/dist/img/avatar.png' />
-          <List.Content>
-            Lena
-          </List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Content floated='right'>
-            <Button>View</Button>
-          </List.Content>
-          <Image avatar src='/assets/images/avatar/small/lindsay.png' />
-          <List.Content>
-            Lindsay
-          </List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Content floated='right'>
-            <Button>View</Button>
-          </List.Content>
-          <Image avatar src='/assets/images/avatar/small/mark.png' />
-          <List.Content>
-            Mark
-          </List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Content floated='right'>
-            <Button>View</Button>
-          </List.Content>
-          <Image avatar src='/assets/images/avatar/small/molly.png' />
-          <List.Content>
-            Molly
-          </List.Content>
-        </List.Item>
-      </List>
-    </Accordion.Content>
-    </Accordion>
-    </Card>
-        <Card>
       <Card.Content>
-        <Card.Header>Matthew Harris</Card.Header>
-        <Card.Meta>Co-Worker</Card.Meta>
-        <Card.Description>Matthew is a pianist living in Nashville.</Card.Description>
+        <Card.Header>ASSOICATE</Card.Header>
+        <Card.Meta className="associateUser"></Card.Meta>
       </Card.Content>
        <Accordion>
     <Accordion.Title>
       <Icon name='dropdown' />
-      More Info?
+      Memebers
     </Accordion.Title>
     <Accordion.Content>
       <List divided verticalAlign='middle'>
@@ -380,18 +216,16 @@ const Dashboard = () => (
     </Accordion.Content>
     </Accordion>
     </Card>
-
 
     <Card>
       <Card.Content>
-        <Card.Header content='Jake Smith' />
-        <Card.Meta content='Musicians' />
-        <Card.Description content='Jake is a drummer living in New York.' />
+        <Card.Header>WAITING LIST</Card.Header>
+        <Card.Meta className="waitingListUser"></Card.Meta>
       </Card.Content>
        <Accordion>
     <Accordion.Title>
       <Icon name='dropdown' />
-      More Info?
+      Members
     </Accordion.Title>
     <Accordion.Content>
       <List divided verticalAlign='middle'>
@@ -436,7 +270,111 @@ const Dashboard = () => (
     </Accordion>
     </Card>
 
-    
+    <Card>
+      <Card.Content>
+      <Card.Header>HONORARY</Card.Header>
+      <Card.Meta className="honorary"></Card.Meta> 
+      </Card.Content> 
+       <Accordion>
+    <Accordion.Title>
+      <Icon name='dropdown' />
+      Members
+    </Accordion.Title>
+    <Accordion.Content>
+      <List divided verticalAlign='middle'>
+        <List.Item>
+          <List.Content floated='right'>
+            <Button>View</Button>
+          </List.Content>
+          <Image avatar src='../public/dist/img/avatar.png' />
+          <List.Content>
+            Lena
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Content floated='right'>
+            <Button>View</Button>
+          </List.Content>
+          <Image avatar src='/assets/images/avatar/small/lindsay.png' />
+          <List.Content>
+            Lindsay
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Content floated='right'>
+            <Button>View</Button>
+          </List.Content>
+          <Image avatar src='/assets/images/avatar/small/mark.png' />
+          <List.Content>
+            Mark
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Content floated='right'>
+            <Button>View</Button>
+          </List.Content>
+          <Image avatar src='/assets/images/avatar/small/molly.png' />
+          <List.Content>
+            Molly
+          </List.Content>
+        </List.Item>
+      </List>
+    </Accordion.Content>
+    </Accordion>
+    </Card>
+
+    <Card>
+      <Card.Content>
+        <Card.Header>A & P</Card.Header>
+        <Card.Meta className="ap"></Card.Meta> 
+      </Card.Content>
+       <Accordion>
+    <Accordion.Title>
+      <Icon name='dropdown' />
+      Members
+    </Accordion.Title>
+    <Accordion.Content>
+      <List divided verticalAlign='middle'>
+        <List.Item>
+          <List.Content floated='right'>
+            <Button>View</Button>
+          </List.Content>
+          <Image avatar src='../public/dist/img/avatar.png' />
+          <List.Content>
+            Lena
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Content floated='right'>
+            <Button>View</Button>
+          </List.Content>
+          <Image avatar src='/assets/images/avatar/small/lindsay.png' />
+          <List.Content>
+            Lindsay
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Content floated='right'>
+            <Button>View</Button>
+          </List.Content>
+          <Image avatar src='/assets/images/avatar/small/mark.png' />
+          <List.Content>
+            Mark
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Content floated='right'>
+            <Button>View</Button>
+          </List.Content>
+          <Image avatar src='/assets/images/avatar/small/molly.png' />
+          <List.Content>
+            Molly
+          </List.Content>
+        </List.Item>
+      </List>
+    </Accordion.Content>
+    </Accordion>
+    </Card>  
   </Card.Group>
 
 )
